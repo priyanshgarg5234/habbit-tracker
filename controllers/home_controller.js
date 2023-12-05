0ad0c1a81983a4ad7c5ed0dee8c83681a19686c2
priyanshgarg5234 <66772817+priyanshgarg5234@users.noreply.github.com>
2023-12-06 01:07:37 +0530

    Add files via upload

---
+++ controllers/home_controller.js
@@ -0,0 +1,15 @@
+module.exports.home = function(req, res){
+    return res.render('home')
+}
+
+module.exports.create = function(req, res){
+    return res.end('<h1></h1>')
+}
+
+module.exports.toggleStatus = function(req, res){
+    return res.end('<h1></h1>')
+}
+
+module.exports.delete = function(req, res){
+    return res.end('<h1></h1>')
+}
---
+++ controllers/weekView.js
@@ -0,0 +1,7 @@
+module.exports.toggleStatus = function(req, res){
+    return res.end('<h1></h1>')
+}
+
+module.exports.weekView = function(req, res){
+    return res.end('<h1></h1>')
+}
